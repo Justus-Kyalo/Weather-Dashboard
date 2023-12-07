30db09b918ce7dc87cc9f388777660550843d168
RemmyKyalo <chaloremmy@gmail.com>
2023-12-07 14:26:17 +0300

    added styles and changed current weather image depending on current weather not placeholder

--- src/components/DailyWeather/DailyWeather.jsx
+++ src/components/DailyWeather/DailyWeather.jsx
@@ -1,24 +1,45 @@
 import React from "react";
 import "./DailyWeather.css";
+import { IoIosPartlySunny } from "react-icons/io";
 const DailyWeather = ({ data }) => {
+  const regionNames = new Intl.DisplayNames(["en"], { type: "region" });
+
   return (
-    <div className="Dailyweather">
+    <div className="DailyWeather">
       <div className="current-weather">
-        <div>
-          <h4>{data.location.name}</h4>
-          <p>{data.location.country}</p>
+        <div className="weather-image">
+          <img src={`${data.weather[0].main.toLowerCase()}.png`} alt="" />
         </div>
         <div>
-          <h4>{data.current.temp_c}</h4>
+          <h3>{data.name}</h3>
+          <p>{regionNames.of(data.sys.country)}</p>
+        </div>
+        <div>
+          <h3>{data.main.temp}&deg;</h3>
           <p>Temperature</p>
         </div>
         <div>
-          <h4>{data.current.humidity}%</h4>
+          <h3>
+            {data.main.humidity}
+            <span className="units">%</span>{" "}
+          </h3>
           <p>Humididty</p>
         </div>
         <div>
-          <h4>{data.current.wind_kph}</h4>
+          <h3>
+            {data.wind.speed}
+            <span className="units">km/h</span>
+          </h3>
           <p>Wind Speed</p>
+        </div>
+      </div>
+      <div className="daily-forecast">
+        <div>
+          <p>09 am</p>
+          <div className="icons">
+            <IoIosPartlySunny />
+          </div>
+          <p>17</p>
         </div>
       </div>
     </div>
